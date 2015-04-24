9eb28fb68d06b0e07f44006c28fddb5fb880e926
Chelsea Worrel <chelseaworrel@gmail.com>
2015-04-24 13:38:47 -0600

    started customer business intel

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -27,4 +27,13 @@ class CustomerTest < Minitest::Test
     assert customer.transactions[0].is_a?(Transaction)
     assert_equal 4 , customer.transactions.length
   end
+
+  def test_it_can_find_successful_customer_transactions
+    engine = SalesEngine.new("./data")
+    engine.startup
+    customer = engine.customer_repository.find_by_id(80)
+
+    assert_equal 10, customer.transactions.length
+    assert_equal 9 , customer.successful_customer_transactions.length
+  end
 end
