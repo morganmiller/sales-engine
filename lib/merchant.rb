0ea39c47077ed39eecbcda25f5c378de590a9993
Morgan Miller <m.miller722@gmail.com>
2015-04-24 08:27:38 -0600

    Added comments to figure out what the f I did

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -23,6 +23,8 @@ class Merchant
     repository.find_invoices(id)
   end
 
+  #Returns an array of invoice ids to compare to the transactions_invoice_ids_collection
+  #To find missing invoice ids
   def invoice_id_collection
     invoices.map do |inv|
       inv.id
@@ -33,11 +35,14 @@ class Merchant
     repository.find_transactions(invoices)
   end
 
+  #Removes empty arrays from transactions collection of sub-arrays
+  #Then maps an array of invoice ids that DID have transaction objects associated with them
   def transactions_invoice_id_collection
     remainder = transactions.delete_if { |t| t == [] }
     remainder.map { |t| t[0].invoice_id }
   end
 
+  #Saves the invoice ids that don't have any associated transactions with them
   def missing_invoice_ids
     invoice_id_collection.select{ |id| !transactions_invoice_id_collection.include?(id) }
   end
@@ -62,6 +67,7 @@ class Merchant
     customers.max_by { |freq| customers_and_frequency[freq] }
   end
 
+  #Creates a hash that pairs invoice ids with all transactions
   def transactions_by_invoice_id
     transactions_by_inv_id = {}
     transactions.each do |trans|
@@ -71,16 +77,21 @@ class Merchant
     transactions_by_inv_id
   end
 
+  #Takes the hash of invoice ids and transactions, and deletes any invoice ids that
+  #have at least one successful transaction
   def pulled_transactions
     transactions_by_invoice_id.delete_if do |invoice_id, transactions|
       transactions.any? { |a| a.successful? } || invoice_id.nil?
     end
   end
 
+  #Takes remaining invoice ids with FAILED transactions
+  #And reunites them with the invoice ids that had NO transactions
   def all_missing_invoice_ids
     [pulled_transactions.keys, missing_invoice_ids].flatten
   end
 
+  
   def customers_with_pending_invoices
     repository.retrieve_customers_with_pending_invoices(all_missing_invoice_ids)
   end
